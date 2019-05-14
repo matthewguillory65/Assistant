e27132564117d8856fc53dd3f8c4bbbaf12a9f80
matthewguillory65 <matthewguillory65@gmail.com>
2019-05-14 16:59:32 -0500

    TurretBehavior was made
    Testing script for VampiRe. Behavior for a guard while the player has the ability to hide behind objects

---
+++ Assets/Scripts/EnemySight.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+    public float fieldOfViewAngle = 110f;
+    public bool playerInSight;
+    public Vector3 personalLastSighting;
+
+
+}
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthandDamage : MonoBehaviour {
+public class Health : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
