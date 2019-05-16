25b7e611279a962d5eb81e28a0806c9b312da03d
Matthew Williamson <chuwilliamson@gmail.com>
2019-05-16 09:02:38 -0500

    add Context Base class

---
+++ Assets/Scripts/Chu/Context.cs
@@ -0,0 +1,10 @@
+﻿namespace Chu
+{
+    public abstract class Context : IContext
+    {
+        public abstract void ResetContext();
+        public abstract IState CurrentState { get; }
+        public abstract void UpdateContext();
+        public abstract void ChangeState(IState next);
+    }
+}
