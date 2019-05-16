ce296499eb9970eaadfd907d4d6e17da663c4f2a
matthewguillory65 <matthewguillory65@gmail.com>
2019-05-16 13:29:36 -0500

    Addition for the npc to not be able to see the player from behind a wall.

---
+++ Assets/Scripts/Rain/Behaviour/ClosestPointCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rain.Behaviour
+{
+    public class ClosestPointCollider : MonoBehaviour
+    {
+        public Vector3 location;
+        public new Collider collider;
+        public static Vector3 closestpoint;
+        void Start()
+        {
+            collider = GetComponent<Collider>();
+        }
+
+        void Update()
+        {
+            closestpoint = collider.ClosestPoint(location);
+        }
+
+    }
+}
+
+
--- Assets/Scripts/Rain/Behaviour/TurretBehavior.cs
+++ Assets/Scripts/Rain/Behaviour/TurretBehavior.cs
@@ -11,26 +11,24 @@ namespace Rain.Behaviour
         bool inRange;
         LayerMask layermask = -1;
         RaycastHit hit;
-        public Vector3 location;
-        public new BoxCollider collider;
     
         public GameObject line1, line2;
         void Start()
         {
-            collider = GetComponent<BoxCollider>();
+            
         }
         void Update()
         {
-            Vector3 closestPoint = collider.ClosestPoint(location);
+            var closestPoint = ClosestPointCollider.closestpoint;
             targetTran = player.transform.position;
             inRange = Vector3.Distance(transform.position, targetTran) < maxRange && Vector3.Distance(transform.position, targetTran) > minRange;
             var inFront = Vector2.Dot(player.transform.forward, transform.forward) < 0;
-            if (inRange && Vector3.Distance(player.transform.position, transform.position) < Vector3.Distance(closestPoint, transform.position))
+            if (inRange && Vector3.Distance(player.transform.position, transform.position) <= Vector3.Distance(closestPoint, line1.transform.position) && Vector3.Distance(player.transform.position, transform.position) <= Vector3.Distance(closestPoint, line2.transform.position))
             {
-                transform.LookAt(targetTran);
                 if (!Physics.Raycast(transform.position, new Vector3(line1.transform.position.x, 0, line1.transform.position.x), out hit, 8)
                     && !Physics.Raycast(transform.position, new Vector3(line2.transform.position.x, 0, line2.transform.position.x), out hit, 8))
                 {
+                    transform.LookAt(targetTran);
                     transform.Translate(Vector3.forward * Time.deltaTime);
                     Debug.Log("hit");
                 }
